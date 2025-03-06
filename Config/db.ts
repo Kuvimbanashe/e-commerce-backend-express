747be17fd9997099ea050cd2c9045c208b3496d1
Kuvimbanashe <camaradery303@gmail.com>
2025-03-06 16:12:22 +0200

    Update db.ts

--- Config/db.ts
+++ Config/db.ts
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose');
 
-try{
-    mongoose.connect('mongodb://localhost:27017/ecommerce', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-    });
-    console.log('Database connected');
-
-}
-catch(error){
-    if (error instanceof Error) {
-        console.log(error.message);
-    } else {
-        console.log(String(error));
+const DbConnect = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log('Database connected');
+    
+    }
+    catch(error){
+        if (error instanceof Error) {
+            console.log(error.message);
+        } else {
+            console.log(String(error));
+        }
     }
 }
