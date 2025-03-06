ed00dd4e95b54bb3b13f935d2eb785417a346cc1
Kuvimbanashe <camaradery303@gmail.com>
2025-03-06 15:56:15 +0200

    Create Cart.ts

---
+++ Carts/model/Cart.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ICart extends Document {
+  user: mongoose.Schema.Types.ObjectId;
+  items: { product: mongoose.Schema.Types.ObjectId; quantity: number }[];
+  createdAt: Date;
+}
+
+const CartSchema = new Schema<ICart>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    items: [
+      {
+        product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+        quantity: { type: Number, required: true, min: 1 },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<ICart>("Cart", CartSchema);
