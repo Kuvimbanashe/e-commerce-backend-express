1f95e019383895f1726a69ec86c51567a755f091
Kuvimbanashe <camaradery303@gmail.com>
2025-03-06 15:58:22 +0200

    Create Category.ts

---
+++ Categories/model/Category.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ICategory extends Document {
+  name: string;
+  description?: string;
+  createdAt: Date;
+}
+
+const CategorySchema = new Schema<ICategory>(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<ICategory>("Category", CategorySchema);
