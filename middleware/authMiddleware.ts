2badb1e7f12589dea4dd35be2747f87da807a15c
Kuvimbanashe <camaradery303@gmail.com>
2025-03-06 16:18:21 +0200

    Create authMiddleware.ts

---
+++ middleware/authMiddleware.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface AuthRequest extends Request {
+  user?: any;
+}
+
+const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const token = req.cookies.token || req.header("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Access Denied! No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+export default authMiddleware;
