b3e4bac423ce5fc0951a34e8aeb7b6da5c3e7613
Kuvimbanashe <camaradery303@gmail.com>
2025-03-20 13:52:31 +0200

    Create users.controller.js

---
+++ Users/controllers/users.controller.js
@@ -0,0 +1,24 @@
+const User = require('../model/user.model');
+
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+const createUser = async (req, res) => {
+    const user = new User(req.body);
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+module.exports = { getUsers, createUser };
