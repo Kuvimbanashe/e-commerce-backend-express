3f8dc48cdc7ec00d53530c5bedd89beffbdb9a7e
Kuvimbanashe <camaradery303@gmail.com>
2025-03-20 15:31:49 +0200

    Update order.controller.ts

--- Orders/controllers/order.controller.ts
+++ Orders/controllers/order.controller.ts
@@ -1,11 +1,5 @@
 import Order from "../model/Order";
-import { Request, Response } from "express";
-
-//  user: mongoose.Schema.Types.ObjectId;
-//   products: { product: mongoose.Schema.Types.ObjectId; quantity: number }[];
-//   totalPrice: number;
-//   status: "pending" | "shipped" | "delivered" | "cancelled";
-
+import e, { Request, Response } from "express";
 
 
 const getOrders = async (req: Request, res: Response): Promise<void> => {
@@ -38,6 +32,64 @@ const createOrder = async (req: Request, res: Response): Promise<void> => {
 };
 
 
+const getOrderById = async (req: Request, res: Response): Promise<void> => {
+    const { id } = req.params;
+    if (!id) {
+        res.status(400).json({ message: 'Id is missing' });
+    }
+    try {
+        const order = await Order.findById(id);
+        if (!order) {
+            res.status(404).json({ message: 'Order not found' });
+        }
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(404).json({ message: (error as Error).message });
+    }
+}
 
+
+const updateOrder = async (req: Request, res: Response): Promise<void> => {
+
+    const { id } = req.params;
+
+    const { user, products, totalPrice, status } = req.body;
+    if (!id) {
+        res.status(400).json({ message: 'Id is missing' });
+    }
+
+    try {
+        const order = await Order.findByIdAndUpdate(id, { user, products, totalPrice, status }, { new: true });
+        if (!order) {
+            res.status(404).json({ message: 'Order not found' });
+        }
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(404).json({ message: (error as Error).message });
+    }
+}
+
+
+const deleteOrder = async (req: Request, res: Response): Promise<void> => {
+
+    const { id } = req.params;
+
+    if (!id) {
+        res.status(400).json({ message: 'Id is missing' });
+    }
+
+    try {
+        const order = await Order.findByIdAndDelete(id);
+        if (!order) {
+            res.status(404).json({ message: 'Order not found' });
+        }
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(404).json({ message: (error as Error).message });
+    }
+}
+
+
+export { getOrders, createOrder, getOrderById, updateOrder, deleteOrder };
 
 
