8584c8bdc17ffe05906829e2da941462ea5c5ae2
Kuvimbanashe <camaradery303@gmail.com>
2025-03-20 15:09:27 +0200

    Update review.routes.ts

---
+++ Reviews/routes/review.routes.ts
@@ -0,0 +1,12 @@
+//import the functions from the controller
+import { getReviews, createReview, getReviewById, updateReview, deleteReview } from '../controllers/review.controller';
+import { Router } from 'express';
+
+const router = Router();
+
+
+router.get('/', getReviews);
+router.post('/', createReview);
+router.get('/:id', getReviewById);
+router.put('/:id', updateReview);
+router.delete('/:id', deleteReview);    
