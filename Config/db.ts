d05f9e76c315d3e013a30c55425567c07dd58718
Kuvimbanashe <camaradery303@gmail.com>
2025-03-20 13:52:27 +0200

    Update db.ts

--- Config/db.ts
+++ Config/db.ts
@@ -5,8 +5,7 @@ const DbConnect = async ()=>{
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+           
         });
         console.log('Database connected');
     
