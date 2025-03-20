8d8666b63bd32cc0781eea54aafe693f8a536364
Kuvimbanashe <camaradery303@gmail.com>
2025-03-20 14:11:27 +0200

    Create users.controller.ts

---
+++ Users/controllers/users.controller.ts
@@ -0,0 +1,109 @@
+const User = require('../model/user.model');
+
+
+const getUsers = async (req: any, res: any): Promise<void> => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: (error as Error).message });
+    }
+}
+
+
+const createUser = async (req: any, res: any): Promise<void> => {
+
+    const { name, email, phone , password,role, address} = req.body;
+
+    const missingFields = [];
+    if (!name) missingFields.push('Name');
+    if (!email) missingFields.push('Email');
+    if (!phone) missingFields.push('Phone');
+    if (!password) missingFields.push('Password');
+    if (!role) missingFields.push('Role');
+    if (!address) missingFields.push('Address');
+
+    if (missingFields.length > 0) {
+        return res.status(400).json({ message: `${missingFields.join(', ')} is missing` });
+    }
+    
+    try {
+
+        const user = new User({ name, email, phone , password,role, address});
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: (error as Error).message});
+    }
+}
+
+const getUserById = async (req: any, res: any): Promise<void> => {
+
+    const id = req.params.id;
+    if (!id) {
+        return res.status(400).json({ message: 'Id is missing' });
+    }
+    try {
+        const user = await User.findOne({ _id: id });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({ message: (error as Error).message });
+    }
+}
+
+const updateUser = async (req: any, res: any): Promise<void> => {
+    const id = req.params.id;
+
+    if (!id) {
+        return res.status(400).json({ message: 'Id is missing' });
+    }
+
+    const { name, email, phone , password,role, address} = req.body;
+    
+    const missingFields = [];
+    if (!name) missingFields.push('Name');
+    if (!email) missingFields.push('Email');
+    if (!phone) missingFields.push('Phone');
+    if (!password) missingFields.push('Password');
+    if (!role) missingFields.push('Role');
+    if (!address) missingFields.push('Address');
+
+    if (missingFields.length > 0) {
+        return res.status(400).json({ message: `${missingFields.join(', ')} is missing` });
+    }
+
+    try {
+        const user = await User.findOneAndUpdate({ _id: id }, req.body, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    }
+    catch (error) {
+        res.status(404).json({ message: (error as Error).message });
+    }
+
+}
+
+const deleteUser = async (req: any, res: any): Promise<void> => {
+    const id = req.params.id;
+    if (!id) {
+        return res.status(400).json({ message: 'Id is missing' });
+    }
+    try {
+        const user = await User.findOneAndDelete({ _id: id });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User deleted successfully' });
+    }
+    catch (error) {
+        res.status(404).json({ message: (error as Error).message });
+    }
+}
+
+module.exports = { getUsers, createUser, getUserById, updateUser, deleteUser };
