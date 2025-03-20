0670ed2fd679faa5cf1055de6f4fc869ffe49e0d
Kuvimbanashe <camaradery303@gmail.com>
2025-03-20 15:17:29 +0200

    Create payment.routes.ts

---
+++ Payments/routes/payment.routes.ts
@@ -0,0 +1,10 @@
+import { getPayments, createPayment, getPaymentById, updatePayment, deletePayment } from '../controllers/payment.controllers';
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', getPayments);
+router.post('/', createPayment);
+router.get('/:id', getPaymentById);
+router.put('/:id', updatePayment);
+router.delete('/:id', deletePayment);
