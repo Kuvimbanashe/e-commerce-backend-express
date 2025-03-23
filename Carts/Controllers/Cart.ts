bdc0ae723f0b7e2b6147ac45f2908e9ecfc235a0
Kuvimbanashe <camaradery303@gmail.com>
2025-03-23 20:26:36 +0200

    Update Cart.ts

--- Carts/Controllers/Cart.ts
+++ Carts/Controllers/Cart.ts
@@ -21,3 +21,48 @@ const getCarts = async (req:Request, res:Response) => {
         res.status(500).send()
     }
 }
+
+const deleteCart = async (req:Request, res:Response)=>{
+    const id = req.params.id
+
+    try{
+        if(!id){
+            res.status(400).json({message:"Id required"})
+        }
+    
+        const deletedCart = await Cart.findOneAndDelete({_id:id})
+    
+        if(!deletedCart){
+            res.status(404).json({message :"Cart not found"})
+        }
+    
+        res.status(200).json({message :"Cart deleted"})
+    }
+    catch (error) {
+        res.status(400).json({ message: (error as Error).message });
+    }
+
+}
+
+const updateCart = async (req:Request, res:Response)=>{
+    const id = req.params.id
+
+    try{
+        if(!id){
+            res.status(400).json({message:"Id required"})
+        }
+    
+        const updatedCart = await Cart.findOneAndUpdate({_id:id},req.body,{new:true})
+    
+        if(!updatedCart){
+            res.status(404).json({message :"Cart not found"})
+        }
+    
+        res.status(200).json({message :"Cart updated"})
+    }
+    catch (error) {
+        res.status(400).json({ message: (error as Error).message });
+    }
+
+}
+
